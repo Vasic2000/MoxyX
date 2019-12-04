5d85357ab09c270591b5d9e5256255ef87c2e5a6
Vasic2000 <Vasic2000@yandex.ru>
2019-12-04 14:20:58 +0300

    Init

---
+++ app/src/main/java/com/example/moxy_x/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.moxy_x;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.arellomobile.mvp.MvpAppCompatActivity;
+import com.arellomobile.mvp.presenter.InjectPresenter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends MvpAppCompatActivity implements MainView {
+
+    @BindView(R.id.btnCounter1)
+    Button buttonOne;
+
+    @BindView(R.id.btnCounter2)
+    Button buttonTwo;
+
+    @BindView(R.id.btnCounter3)
+    Button buttonThree;
+
+    @InjectPresenter
+    MainPresenter presenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnCounter1, R.id.btnCounter2, R.id.btnCounter3})
+    public void onButtonClick(Button button) {
+        presenter.counterClick(button.getId());
+    }
+
+    @Override
+    public void setButtonText(int index, int value) {
+        switch (index) {
+            case 0:
+                buttonOne.setText(String.format(getString(R.string.count_format), value));
+                break;
+            case 1:
+                buttonTwo.setText(String.format(getString(R.string.count_format), value));
+                break;
+            case 2:
+                buttonThree.setText(String.format(getString(R.string.count_format), value));
+                break;
+        }
+    }
+}
